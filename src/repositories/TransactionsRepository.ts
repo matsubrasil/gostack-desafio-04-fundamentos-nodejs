4250e2831edd1350b41fd5ca5374f6ed3dec3780
matsubrasil <matsuboy@outlook.com>
2020-06-28 19:14:54 -0300

    add create transaction and show transactions and balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,27 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balanceInitial = { income: 0, outcome: 0, total: 0 };
+
+    let balance = this.transactions.reduce((r, a) => {
+      r[a.type] = r[a.type] + a.value;
+      a.type === 'income' ? (r['total'] += a.value) : (r['total'] -= a.value);
+      return r;
+    }, balanceInitial);
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
